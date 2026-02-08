8654ff63ff4c4046b1f2176a83ba7c2d1f80a144
Shmulik Flint <splintor@gmail.com>
2026-02-08 22:09:38 +0200

    update dependencies

--- remix.config.js
+++ remix.config.js
@@ -5,4 +5,12 @@ export default {
   // assetsBuildDirectory: "public/build",
   // publicPath: "/build/",
   // serverBuildPath: "build/index.js",
+
+  future: {
+    v3_fetcherPersist: true,
+    v3_lazyRouteDiscovery: true,
+    v3_relativeSplatPath: true,
+    v3_singleFetch: true,
+    v3_throwAbortReason: true,
+  },
 };
